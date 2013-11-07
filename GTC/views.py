e03b64b2756f49a0008d417d5912995b790fc644
Matt Lodes <lodes.matt@gmail.com>
2013-11-06 21:56:32 -0600

    generic form to get user strings

--- GTC/views.py
+++ GTC/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 import datetime
 from django.shortcuts import render
-
+from customforms.contact import StringForm
 
 def hello(request):
     return HttpResponse("Hello world")
@@ -14,5 +14,19 @@ def current_datetime(request):
 
 
 def index(request):
-
     return render(request, 'index.html')
+
+
+def calculate(request):
+    if request.method == 'POST': # If the form has been submitted...
+        form = StringForm(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            # Process the data in form.cleaned_data
+            # ...
+            return HttpResponseRedirect('/thanks/') # Redirect after POST
+    else:
+        form = StringForm() # An unbound form
+
+    return render(request, 'calculate.html', {
+        'form': form,
+    })
--- GuitarTensionCalculator/urls.py
+++ GuitarTensionCalculator/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from GTC.views import hello, current_datetime, index
+from GTC.views import hello, current_datetime, index, calculate
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -8,6 +8,8 @@ urlpatterns = patterns('',
    url(r'^hello/$', hello),
    url(r'^time/$', current_datetime),
    url(r'^index/$', index),
+   url(r'^calculate/$', calculate),
+
 
    # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
