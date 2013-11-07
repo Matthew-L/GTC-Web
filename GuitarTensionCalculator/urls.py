e8774beb84df190ce2a42b7695cd04d85b8d6c70
Matt Lodes <lodes.matt@gmail.com>
2013-11-06 20:59:35 -0600

    added hello page

--- GuitarTensionCalculator/urls.py
+++ GuitarTensionCalculator/urls.py
@@ -1,15 +1,13 @@
 from django.conf.urls import patterns, include, url
-
+from GTC.views import hello
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'GuitarTensionCalculator.views.home', name='home'),
-    # url(r'^GuitarTensionCalculator/', include('GuitarTensionCalculator.foo.urls')),
+   url(r'^hello/$', hello),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
+   # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
