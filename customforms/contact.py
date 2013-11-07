73f9e0837eaad5f108690132f951023f92f80a4c
Matt Lodes <lodes.matt@gmail.com>
2013-11-06 22:48:15 -0600

    User input works, waiting on tension function

--- customforms/contact.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'mattlodes'
-from django import forms
-
-
-class StringForm(forms.Form):
-    Octave = forms.ChoiceField(choices=[(x, x) for x in range(0, 10)])
-    String_Type = forms.CharField()
-    Metric = forms.BooleanField(required=False)
-    Note = forms.ChoiceField(choices=[(x, x) for x in range(1, 32)])
