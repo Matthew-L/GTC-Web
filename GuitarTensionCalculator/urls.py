175bcc7bd6e431c6587b21d0d87f8d62413e67ec
Matt Lodes <lodes.matt@gmail.com>
2013-11-06 21:16:16 -0600

    includes work

--- GuitarTensionCalculator/urls.py
+++ GuitarTensionCalculator/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, include, url
-from GTC.views import hello
+from GTC.views import hello, current_datetime, index
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
    url(r'^hello/$', hello),
+   url(r'^time/$', current_datetime),
+   url(r'^index/$', index),
 
    # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
