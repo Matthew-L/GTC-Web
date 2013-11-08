6323d0dc5154f22168f66de3acf67dcddc7c54ff
Matt Lodes <lodes.matt@gmail.com>
2013-11-08 02:12:28 -0600

    Templates with articles work

--- article/views.py
+++ article/views.py
@@ -36,7 +36,7 @@ def articles(request):
 
 
 def article(request, article_id=1):
-    return render_to_response('articles.html', {'articles': Article.objects.get(id=article_id)})
+    return render_to_response('article.html', {'article': Article.objects.get(id=article_id)})
 
 #####################################################
 
