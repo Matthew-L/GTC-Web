b9bfe883f73cbd1fc42f0cb25d7d87ced18b8b5a
Matt Lodes <lodes.matt@gmail.com>
2013-11-08 07:01:24 -0600

    working version

---
+++ customforms/string.py
@@ -0,0 +1,15 @@
+__author__ = 'mattlodes'
+from django import forms
+
+
+
+ACCEPTED_NOTES = ['A', 'C', 'C#/Db', 'D', 'Eb/D#', 'E', 'F', 'F#/Gb', 'G', 'Ab/G#']
+OCTAVE_RANGE = 11
+STRING_TYPE = [("PL","Plain"), ("PB","Phosphorus Bronze"), ("NW","Nickel Wound"), ("XS", "XS"), ("HR","Half-Round")]
+
+class StringForm(forms.Form):
+    Scale_Length = forms.FloatField()
+    Octave = forms.ChoiceField(choices=[(octave, octave) for octave in range(OCTAVE_RANGE)])
+    String_Type = forms.ChoiceField(choices=STRING_TYPE)
+    Note = forms.ChoiceField(choices=[(index, index) for index in ACCEPTED_NOTES])
+    Gauge = forms.FloatField()
---
+++ pythonbackend/views.py
@@ -0,0 +1,86 @@
+
+from django.shortcuts import render
+from customforms.string import StringForm
+from calculator.calculate import GTC
+import ast
+
+
+
+"""
+Sends the form that gets the user input for the strings
+"""
+def calculate(request):
+    form = StringForm() # An unbound form
+    return render(request, 'calculate.html', {'form': form,})
+
+
+
+
+
+"""
+Generates the view that displays the tension or an error page there is an error in the input
+"""
+def results(request):
+    GET_PARAMETERS = ["String_Type", "Octave", "Gauge", "Scale_Length"]
+    key = request.GET.keys()
+    print(key)
+    for parameter in GET_PARAMETERS:
+        print(parameter)
+        if parameter not in key:
+            return render(request, 'input_error.html')
+
+    if is_valid_result(request.GET):
+        scale_length = ast.literal_eval(request.GET["Scale_Length"])
+        string_material = request.GET["String_Type"]
+        gauge = ast.literal_eval(request.GET["Gauge"])
+        note = request.GET["Note"]
+        octave = ast.literal_eval(request.GET["Octave"])
+        guitar_string = GTC(scale_length, string_material, gauge, note, octave)
+        context = {'tension': guitar_string.calculate_tension()}
+        return render(request, 'results.html', context)
+
+    return render(request, 'input_error.html')
+
+
+
+
+VALIDATE_PARAMETERS = ["String_Type", "Octave", "Gauge", "Scale_Length"]
+ACCEPTED_NOTES = ['A', 'C', 'C#/Db', 'D', 'Eb/D#', 'E', 'F', 'F#/Gb', 'G', 'Ab/G#']
+OCTAVE_RANGE = 11
+STRING_TYPE = ["PL", "PB", "NW", "XS", "HR"]
+
+
+
+
+"""
+    Checks that users input is valid
+    result = Dictionary of data to be sent to GTC
+    return boolean if result is valid
+"""
+def is_valid_result(result):
+    print("In is_valid_result")
+    for parameter in VALIDATE_PARAMETERS:
+        if parameter not in result.keys():
+            return False
+
+    if result['String_Type'] not in STRING_TYPE:
+        return False
+
+    gauge = result['Gauge']
+    if gauge.isdigit():
+        if float(result['Gauge']) < 0:
+            return False
+    else:
+        return False
+
+    if result['Note'] not in ACCEPTED_NOTES:
+        return False
+
+    scale_length = result['Scale_Length']
+    if scale_length.isdigit():
+        if float(result['Scale_Length']) < 0:
+            return False
+    else:
+        return False
+
+    return True
