e69d1d9399995a519c3e466d1baa2998a6f8ade1
Matt Lodes <lodes.matt@gmail.com>
2013-11-08 07:20:47 -0600

    test cases

--- pythonbackend/tests.py
+++ pythonbackend/tests.py
@@ -6,7 +6,7 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-
+from pythonbackend.views import is_valid_result
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -14,3 +14,20 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+    def test_scale_length(self):
+        self.assertFalse(is_valid_result(("1w1", "PL", 1, "A", 2)))
+        self.assertFalse(is_valid_result(("w", "PL", 1, "A", 2)))
+        self.assertFalse(is_valid_result(("-1", "PL", 1, "A", 2)))
+
+        self.assertTrue(is_valid_result(("2", "PL", 1, "A", 2)))
+
+    def test_gauge_length(self):
+        self.assertFalse(is_valid_result((1, "PL", "1w1", "A", 2)))
+        self.assertFalse(is_valid_result((1, "PL", "w", "A", 2)))
+        self.assertFalse(is_valid_result((1, "PL", -1, "A", 2)))
+
+        self.assertTrue(is_valid_result((1, "PL", 1, "A", 2)))
+
+
+
