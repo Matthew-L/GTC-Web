c179ba0291ffd0fbee769b574328501fd2053113
Micah <gajewsk2@illinois.edu>
2013-11-15 00:34:36 -0600

    schema complete

--- customforms/string.py
+++ customforms/string.py
@@ -1,11 +1,11 @@
 __author__ = 'mattlodes'
 from django import forms
 
-
-
 ACCEPTED_NOTES = ['A', 'A#/Bb', 'B', 'C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab']
 OCTAVE_RANGE = 11
-STRING_TYPE = [("PL","Plain"), ("PB","Phosphorus Bronze"), ("NW","Nickel Wound"), ("XS", "Stainless Steel"), ("HR","Half-Round")]
+STRING_TYPE = [("PL", "Plain"), ("PB", "Phosphorus Bronze"),
+               ("NW", "Nickel Wound"), ("XS", "Stainless Steel"), ("HR", "Half-Round")]
+
 
 class StringForm(forms.Form):
     Scale_Length = forms.FloatField()
--- pythonbackend/models.py
+++ pythonbackend/models.py
@@ -1,9 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class strings(models.Model):
-    username = models.CharField()
-    scale_length = models.DecimalField()
+class StringSet(models.Model):
+    name = models.CharField()
+    user = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+
+class String(models.Model):
+    string_set = models.ForeignKey(StringSet)
+    string_number = models.IntegerField()
+    scale_length = models.CharField(max_length=30)
     note = models.CharField()
     octave = models.IntegerField()
     gauge = models.DecimalField()
