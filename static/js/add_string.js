148b1f9e6f3e0b6a5d5ea1bb62c784ebdb5e4860
Micah Gajewski <gajewsk2@illinois.edu>
2013-11-21 15:00:46 -0600

    finished html form

--- static/js/add_string.js
+++ static/js/add_string.js
@@ -3,11 +3,23 @@
  */
 
     $(document).ready(function() {
+
         $("#string_row0").nextAll().hide();
-        var counter = 1;
+        var counter = 0;
         $(".addstring_btn").click(function() {
-            $("#string_row"+counter).toggle("slide")
+            var row = "#string_row"+counter
+            $(row).show("slide")
+//            alert($(row).load( "../templates/includes/forms.html" ));
             counter += 1;
+        })
+
+        $(".remove_btn").click(function() {
+            console.log('hi')
+            var curr = this.id
+            $("#string_row"+curr.substr(curr.length - 1)).toggle();
+//            alert("#string_row"+curr.substr(curr.length - 1))
+//            counter -= 1;
+//            $("#string_row"+id_num).toggle("slide")
         })
     });
 
