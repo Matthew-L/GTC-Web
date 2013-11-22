7196c89facd72bd18c75b73d35710110d3e52034
Micah Gajewski <gajewsk2@illinois.edu>
2013-11-21 23:52:42 -0600

    jquery catches all events, starting ajax

--- static/js/add_string.js
+++ static/js/add_string.js
@@ -15,12 +15,55 @@
 
         $(".remove_btn").click(function() {
             console.log('hi')
-            var curr = this.id
-            $("#string_row"+curr.substr(curr.length - 1)).toggle();
+            var curr = this.id.substr(this.id.length - 1)
+            $("#string_row"+curr).toggle();
 //            alert("#string_row"+curr.substr(curr.length - 1))
 //            counter -= 1;
 //            $("#string_row"+id_num).toggle("slide")
         })
+
+        $(".row_input").on("change", calculate)
     });
 
 
+function calculate(){
+    var name = $("#string_set_name").val();
+    var scale_length = $("#scale_length").val();
+    if(name != '' && scale_length != ''){
+        var curr = this.id.substr(this.id.length - 1);
+        var string_number = $("#string_number"+curr).val();
+        var note = $("#note"+curr).val();
+        var octave = $("#octave"+curr).val();
+        var gauge = $("#gauge"+curr).val();
+        var string_type = $("#string_type"+curr).val();
+
+        if(string_number != '' && note != '-'
+            && octave != '-' && gauge != ''
+            && string_type != '-' && name != ''
+            && scale_length != ''){
+                console.log("yeay");
+        //$.ajax({
+        //    type: "POST",
+        //    url: "/ajax",
+        //    data: {
+        //        key: 1,
+        //        text: new_text
+        //    },
+        //    dataType: "json",
+        //    success: function(response){
+        //        $("#editor").hide()
+        //        $("#save").hide();
+        //        $("#cancel").hide();
+        //
+        //        response_text = response.message;
+        //        $("#text").text(response_text);
+        //        $("#text").show();
+        //    },
+        //    error: function(response, error){
+        //        alert("ERROR!");
+        //       }
+        //    })
+        //}
+        }
+    }
+}
