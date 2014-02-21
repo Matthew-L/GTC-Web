e2fd1f18c27f2010d92d829fc948d27d9a306be4
gajewsk2 <gajewsk2@illinois.edu>
2014-02-20 19:08:19 -0600

    added popover js file

---
+++ static/js/popover_header_info.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Micah on 2/20/14.
+ */
+
+    $(document).ready(function() {
+
+        $("#string_set_name").popover({ placement: "bottom", animation:"true",  trigger: "hover", title: 'Twitter Bootstrap Popover', content: "It's so simple to create a tooltop for my website!"});
+        $("#desc").popover({ placement: "bottom", animation:"true",  trigger: "hover", title: 'Twitter Bootstrap Popover', content: "It's so simple to create a tooltop for my website!"});
+        $("#mscale").popover({ placement: "bottom", animation:"true",  trigger: "hover", title: 'Twitter Bootstrap Popover', content: "It's so simple to create a tooltop for my website!"});
+        $("#number_of_strings_popover").popover({ placement: "bottom", animation:"true",  trigger: "hover", title: 'Twitter Bootstrap Popover', content: "It's so simple to create a tooltop for my website!"});
+        $("#scale_length_popover").popover({ placement: "bottom", animation:"true",  trigger: "hover", title: 'Twitter Bootstrap Popover', content: "It's so simple to create a tooltop for my website!"});
+        $("#number").popover({ placement: "top", animation:"true",  trigger: "hover", title: 'Twitter Bootstrap Popover', content: "It's so simple to create a tooltop for my website!"});
+        $("#note").popover({ placement: "top", animation:"true",  trigger: "hover", title: 'Twitter Bootstrap Popover', content: "It's so simple to create a tooltop for my website!"});
+        $("#octave").popover({ placement: "top", animation:"true",  trigger: "hover", title: 'Twitter Bootstrap Popover', content: "It's so simple to create a tooltop for my website!"});
+        $("#gauge").popover({ placement: "top", animation:"true",  trigger: "hover", title: 'Twitter Bootstrap Popover', content: "It's so simple to create a tooltop for my website!"});
+        $("#string-type").popover({ placement: "top", animation:"true",  trigger: "hover", title: 'Twitter Bootstrap Popover', content: "It's so simple to create a tooltop for my website!"});
+        $("#tension").popover({ placement: "top", animation:"true",  trigger: "hover", title: 'Twitter Bootstrap Popover', content: "It's so simple to create a tooltop for my website!"});
+
+    });
