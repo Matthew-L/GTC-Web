78bec19d919844530417c2db5bcc7e7e00fe7736
gajewsk2 <gajewsk2@illinois.edu>
2014-07-19 16:43:43 -0500

    fixing wsgi issue

--- stringulator/wsgi.py
+++ stringulator/wsgi.py
@@ -1,5 +1,5 @@
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stringulator.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stringulator")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
