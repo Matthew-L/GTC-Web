de42759c0b35f40f1b0ae0a4d2c7c871b3adbbf5
gajewsk2 <gajewsk2@illinois.edu>
2014-07-19 17:04:45 -0500

    fixed stringulator not being a package

--- stringulator/wsgi.py
+++ stringulator/wsgi.py
@@ -1,5 +1,5 @@
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stringulator")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stringulator.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
