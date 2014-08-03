a689f5d706ca4be31d5f37fb13d1b1e894560582
gajewsk2 <gajewsk2@illinois.edu>
2014-08-03 01:06:56 -0500

    finished length class implementation; 100% coverage for length;

---
+++ calculator/stringcalculator/guitarstring/length.py
@@ -0,0 +1,96 @@
+class InvalidScaleLengthError(ValueError): pass
+
+
+class OutOfRangeError(ValueError): pass
+
+
+class InvalidStringNumberError(ValueError): pass
+
+
+class Length():
+    def __init__(self, scale_length, string_total=None, string_index=None):
+        if str(scale_length).find('-') != -1:
+            if string_total is None or string_index is None:
+                raise InvalidScaleLengthError("Multi-scale scale_lengths (scale_lengths containing \'-\'"
+                                              " must specify the number of strings and string_index")
+            self.string_total = self.sanitize_number(string_total)
+            self.string_index = self.sanitize_number(string_index)
+            if self.string_index > self.string_total:
+                raise OutOfRangeError(' string_index must be less than string_total')
+
+            self.scale_length = self.convert_multiscale_to_scale_length(scale_length, self.string_total,
+                                                                        self.string_index)
+        else:
+            self.scale_length = self.sanitize_scale_length(scale_length)
+
+    def convert_multiscale_to_scale_length(self, scale_length, string_total, string_index):
+        """
+        Takes the scale_length as a str and transforms it into the scale length range (two separate values)
+        By using the total number of strings and the current string's string_index the current strings
+        scale_length is calculated.
+
+        @param scale_length: a string containing two floats separated by a hyphen
+        @param string_total: the total numbers of strings the multi-scale neck spans
+        @param string_index: the current strings number (the higher the string typically the lower the number)
+                                high e on standard would be a 1
+                                low E on standard would be a 6
+        @return: the current strings scale_length
+        """
+        high_scale_length, low_scale_length = self.sanitize_multiscale(scale_length)
+
+        if high_scale_length < low_scale_length:
+            temp = high_scale_length
+            high_scale_length = low_scale_length
+            low_scale_length = temp
+
+        fan_distance = high_scale_length - low_scale_length
+        scale_constant = 0
+        if string_total > 1:
+            scale_constant = fan_distance / (string_total - 1)
+
+        return low_scale_length + (string_index - 1) * scale_constant
+
+    @staticmethod
+    def sanitize_scale_length(scale_length):
+        """
+        converts parameter to an float and raises an appropriate error on failure
+        also checks that the scale length is above zero or it will throw OutOfRangeError
+        @param scale_length:
+        @return: @raise OutOfRangeError:
+        """
+        try:
+            scale_length = float(scale_length)
+        except ValueError:
+            raise InvalidScaleLengthError('scale_length must be a float')
+        if scale_length <= 0:
+            raise OutOfRangeError('scale_length must be a positive number')
+        return scale_length
+
+
+    @staticmethod
+    def sanitize_multiscale(scale_length):
+        try:
+            low_scale_length, high_scale_length = scale_length.split('-')
+            low_scale_length = float(low_scale_length)
+            high_scale_length = float(high_scale_length)
+        except ValueError:
+            raise InvalidScaleLengthError('a multi scale_length must be two positive floats separated by a \'-\'')
+        if high_scale_length <= 0 or low_scale_length <= 0:
+            raise OutOfRangeError('both multi scale_lengths must be a strictly positive number')
+        return high_scale_length, low_scale_length
+
+    @staticmethod
+    def sanitize_number(number):
+        """
+        converts both parameters to an int and raises an appropriate error on failure
+        @param string_total:
+        @param string_index:
+        @return: @raise InvalidStringNumberError:
+        """
+        try:
+            number = int(float(number))
+        except ValueError:
+            raise InvalidStringNumberError('Length numbers must be an integer')
+        if number <= 0:
+            raise OutOfRangeError('Length numbers must be strictly positive')
+        return number
---
+++ calculator/stringcalculator/guitarstring/test_length.py
@@ -0,0 +1,69 @@
+import unittest
+from calculator.stringcalculator.guitarstring.length import Length, OutOfRangeError, InvalidScaleLengthError, \
+    InvalidStringNumberError
+
+
+class TestScientificPitch(unittest.TestCase):
+    """test sanitize_scale_length"""
+    def test_not_a_number_scale_length_input(self):
+        self.assertRaises(InvalidScaleLengthError, Length.sanitize_scale_length, 'a')
+
+    def test_negative_scale_length_input(self):
+        self.assertRaises(OutOfRangeError, Length.sanitize_scale_length, 0)
+
+    def test_valid_scale_length_input(self):
+        self.assertEqual(26.5, Length.sanitize_scale_length(26.5))
+
+    def test_valid_int_scale_length_input(self):
+        self.assertEqual(24, Length.sanitize_scale_length(24))
+
+    """test sanitize_number"""
+    def test_not_a_number_length_number(self):
+        self.assertRaises(InvalidStringNumberError, Length.sanitize_number, 'a')
+
+    def test_valid_length_number(self):
+        self.assertEqual(6, Length.sanitize_number(6))
+
+    def test_negative_length_number_input(self):
+        self.assertRaises(OutOfRangeError, Length.sanitize_number, 0)
+
+    """test sanitize_multiscale"""
+    def test_valid_multi_scale_length_input(self):
+        high_scale_length, low_scale_length = Length.sanitize_multiscale('27-28.625')
+        self.assertEquals(27, low_scale_length)
+        self.assertEquals(28.625, high_scale_length)
+
+    def test_not_a_number_multi_scale_length_input(self):
+        self.assertRaises(InvalidScaleLengthError, Length.sanitize_multiscale, 'a-z')
+
+    def test_negative_multi_scale_length_input(self):
+        self.assertRaises(OutOfRangeError, Length.sanitize_multiscale, '0-12')
+
+    """test Length.scale_length"""
+    def test_valid_init_scale_length(self):
+        length = Length(25.5)
+        self.assertEqual(length.scale_length, 25.5)
+
+    def test_invalid_init_scale_length(self):
+        self.assertRaises(InvalidScaleLengthError, Length, 'a')
+
+    """test multiscale Length.scale_length"""
+    def test_valid_multiscale_first_string_init_scale_length(self):
+        length = Length('25.5-27', 8, 1)
+        self.assertEqual(length.scale_length, 25.5)
+
+    def test_valid_multiscale_middle_string_init_scale_length(self):
+        length = Length('26.5-30', 8, 5)
+        self.assertEqual(length.scale_length, 28.5)
+
+    """test multiscale Length numbers"""
+    def test_invalid_number_of_inputs_init(self):
+        self.assertRaises(InvalidScaleLengthError, Length, '26.5-30', 1)
+
+    def test_invalid_index_greater_than_total(self):
+        self.assertRaises(OutOfRangeError, Length, '26.5-30', 1, 8)
+
+    """test multiscale Length.scale_length"""
+    def test_greater_multiscale_length_first(self):
+        length = Length('30-26.5', 8, 5)
+        self.assertEqual(length.scale_length, 28.5)
