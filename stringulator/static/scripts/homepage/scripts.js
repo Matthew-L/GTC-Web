f62bd605ebeb4ff59a92fa67ef89aabf2d8225c9
Kevin <kevinrestaino@gmail.com>
2014-08-12 21:04:53 -0500

    Homepage CSS cleanup

--- stringulator/static/scripts/homepage/scripts.js
+++ stringulator/static/scripts/homepage/scripts.js
@@ -1,10 +1,10 @@
 //On Page Load Animation
 
-$( document ).ready(function() {
-  $('.header').addClass('animated fadeInDown');
-  $('.content').addClass('animated bounceIn');
-  $('.footer').addClass('animated bounceIn');
-});
+//$( document ).ready(function() {
+//  $('.header').addClass('animated fadeInDown');
+//  $('.content').addClass('animated bounceIn');
+//  $('.footer').addClass('animated bounceIn');
+//});
 
 $( '.morph-button button' ).click(function() {
   $('.header').removeClass('animated fadeInDown');
